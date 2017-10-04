c2289e79878bf5e3c9c7d0dd2fb28df32605f7db
Dshinji137 <hd2358@columbia.edu>
2017-10-04 00:05:22 -0400

    Update Split-array-with-equal-sum.java

--- Split-array-with-equal-sum.java
+++ Split-array-with-equal-sum.java
@@ -8,13 +8,16 @@ class Solution {
             sum[i] = t;
         }
         
+        // separate the array into two parts (left and right)
         for(int i = 3; i < nums.length-3; i++) {
             Set<Integer> set = new HashSet<>();
+            // if left part has two parts equal, put them into hash set.
             for(int j = 1; j < i-1; j++) {
                 if(sum[j-1] == sum[i-1]-sum[j]) {
                     set.add(sum[j-1]);
                 }
             }
+            // if right part has two parts equal, check if there are similar sum in hash set, if so, return true
             for(int k = i+2; k < nums.length-1; k++) {
                 if(sum[k-1]-sum[i] == sum[nums.length-1]-sum[k] && set.contains(sum[k-1]-sum[i])) return true;
             }
